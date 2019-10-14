ba023c10ebfdbdbf45e2d4e78dc0475115373dc3
tyanzhe <tyanzhe@163.com>
2019-10-14 23:42:22 +0800

    優化loading顯示效果， 添加批量刪除loading

--- src/main/java/cn/org/tpeach/nosql/service/LogAreaAppender.java
+++ src/main/java/cn/org/tpeach/nosql/service/LogAreaAppender.java
@@ -23,7 +23,7 @@ public class LogAreaAppender implements Observer {
             LarkLog larkLog = (LarkLog) o;
             LogMsg logMsg = larkLog.getLogMsg();
             Color  color = (Color) arg;
-            LarkFrame.logArea.println(logMsg.getReceiveTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))+" "+logMsg.getMsg(), color);
+//            LarkFrame.logArea.println(logMsg.getReceiveTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))+" "+logMsg.getMsg(), color);
         }catch (Exception e){
             log.error("LogAreaAppender 接受消息异常",e);
         }
